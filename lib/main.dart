c00fecce5823ace94ef70a2ee3d120289e630630
Moamen Elfateh <moamenelfateh2@gmail.com>
2025-05-04 10:03:52 +0300

    edit test folder

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(Quiz());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Quiz();
+  }
 }
